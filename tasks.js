b61a40d0f5a2e424832d56fcff417c4d17225075
Jemima <contatojemima@gmail.com>
2023-03-25 23:53:31 -0300

    JS - Implementação da função cverifiIfListIsEmpty()

--- tasks.js
+++ tasks.js
@@ -20,6 +20,16 @@ const addTaskButton = document.getElementsByTagName("button")[0];
 const taskList = document.getElementById("tasks_list");
 const todoCounterText = document.getElementById("todo_count");
 const doneCounterText = document.getElementById("done_count");
+const emptyTasks = document.getElementById("empty_tasks");
+
+// empty tasks
+function verifyIfListIsEmpty() {
+  if(taskData.length === 0) {
+    emptyTasks.classList.remove("hidden");
+  } else {
+    emptyTasks.classList.add("hidden");
+  }
+}
 
 // counter
 function counter() {
@@ -38,6 +48,7 @@ function counter() {
   doneCounterText.innerText = `${doneCounter}`;
 }
 
+verifyIfListIsEmpty();
 counter();
 
 // create new task element
@@ -107,6 +118,7 @@ function addTask(event) {
 
   addTaskInput.value = '';
   counter();
+  verifyIfListIsEmpty();
 }
 
 // complete task
@@ -182,6 +194,7 @@ function deleteTask(event) {
   taskList.removeChild(taskToDelete);
 
   counter();
+  verifyIfListIsEmpty();
 }
 
 // sync HTML with taskData list
